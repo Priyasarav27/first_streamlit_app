aade0e3d29b9385282652d20a708b7b137ef9060
Priyasarav27 <124286878+Priyasarav27@users.noreply.github.com>
2023-02-02 11:59:21 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 import streamlit
 streamlit.title('My parents new healthy diner')
-streamlit.header('Breakfast menu')
-streamlit.text('Omega 3 and Blueberry Oatmeal')
-streamlit.text('Kale,spinach &Rocket smoothie')
-streamlit.text('Hard boiled free range egg')
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
